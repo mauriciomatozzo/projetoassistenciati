fc9dc1ef290d270cd824a397fd4f001b9a3caafa
Felipe Menegat <felipe_menegat@hotmail.com>
2018-06-04 17:04:53 -0300

    Create mensagemFrufru.js

---
+++ mensagemFrufru.js
@@ -0,0 +1,10 @@
+    function Enviar() {
+    var nome = document.getElementById("nomeid");
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
+   function Limpar(){
+    var erase = document.getElementById("Clean");
+     alert('Formulário apagado!');
+   }
