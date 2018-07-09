c9b7791982b8ed93fafd7bf9c4f8785b39f1ab85
Felipe Menegat <felipe_menegat@hotmail.com>
2018-07-09 18:08:48 -0300

    Update deletar.php

--- deletar.php
+++ deletar.php
@@ -1,1 +1,22 @@
+<?php
+require_once 'conexao.php';
+// pega o ID da URL
+$Id = isset($_GET['Id']) ? $_GET['Id'] : null;
+// valida o ID
+if (empty($Id)) {
+    echo "ID não informado";
+    exit;
+}
+// remove do banco
+$connection = db_connect();
+$sql = "DELETE FROM contatos WHERE Id = :Id";
+$stmt = $connection->prepare($sql);
+$stmt->bindParam(':Id', $Id, PDO::PARAM_INT);
+if ($stmt->execute()) {
+    header('Location: lista.php');
+} else {
+    echo "Erro ao remover";
+    print_r($stmt->errorInfo());
+}
+
 
